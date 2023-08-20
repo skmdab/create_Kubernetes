0bdaf424ed9acc3af67c98366887b8449fed85f7
skmdab <57156958+skmdab@users.noreply.github.com>
2023-08-20 21:48:41 +0530

    update

--- aws_create.sh
+++ aws_create.sh
@@ -39,13 +39,13 @@ echo "Creating $INSTANCENAME server"
 
 INSTANCETYPE=t2.medium
 
-AMI_ID=ami-0f5ee92e2d63afc18
+AMI_ID=ami-04a5a6be1fa530f1c
 
-ZONE=subnet-0e53927bc164113ef
+ZONE=subnet-0a3721f48b6b71c75
 
 COUNTS=1
 
-INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --count $COUNTS --instance-type $INSTANCETYPE --key-name filinta --security-group-ids sg-0ecf5126ea77ec20d --subnet-id $ZONE --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$INSTANCENAME'}]' --query 'Instances[0].InstanceId'  --output text)
+INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --count $COUNTS --instance-type $INSTANCETYPE --key-name filinta --security-group-ids sg-0e8a1d885359e7e03 --subnet-id $ZONE --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$INSTANCENAME'}]' --query 'Instances[0].InstanceId'  --output text)
 
 progress_bar 75
 
